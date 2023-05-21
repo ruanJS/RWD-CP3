c41ff479f0ff31f6a8e96e5ba75fa5ba37eb8e98
Pedro-Ferrari <pedrofcmp@gmail.com>
2023-05-21 03:44:29 -0300

    Atualização final do inputSenha.

--- js/cp3.js
+++ js/cp3.js
@@ -23,7 +23,7 @@ inputSenha.addEventListener("focus", ()=>{
 
 inputSenha.addEventListener("keyup", ()=>{
 
-    const lblSenha = document.querySelector("label[for='inputSenha']");
+    const lblSenha = document.querySelector("label[for='senha']");
     if(inputSenha.value.length < 5){
         inputSenha.setAttribute("style","outline-color: #ff0000;");
         lblSenha.setAttribute("style","color: #ff0000;");
