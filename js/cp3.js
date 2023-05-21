8e1698ca301b7a79d35d3c38e47745698d8101fb
bruno1098 <rm98470@fiap.com.br>
2023-05-21 18:50:01 -0300

    criando um elemento "p" para aparecer erro

--- js/cp3.js
+++ js/cp3.js
@@ -1,4 +1,6 @@
 const inputNome = document.querySelector("#primeiroNome");
+let erroNome;
+
 inputNome.addEventListener("focus", ()=>{
     inputNome.setAttribute("style","outline-color: #ff0000;");
 })
@@ -6,12 +8,33 @@ inputNome.addEventListener("focus", ()=>{
 inputNome.addEventListener("keyup", ()=>{
 
     const lblNome = document.querySelector("label[for='primeiroNome']");
-    if(inputNome.value.length < 5){
+    if(inputNome.value.length ==='' || inputNome.value.length < 5){
         inputNome.setAttribute("style","outline-color: #ff0000;");
         lblNome.setAttribute("style","color: #ff0000;");
+
     }else{
         inputNome.setAttribute("style","outline-color: #00ff00;");
         lblNome.setAttribute("style","color: #00ff00;");
     }
-    //inputEmail.setAttribute("style","outline-color: #ff0000;");
 });
+
+inputNome.addEventListener('blur', ()=>{
+    if (inputNome.value === '' || inputNome.value.length < 5) {
+      mostrarErro('O campo deve conter pelo menos 5 caracteres.');
+    } else {
+      removerErro();
+    }
+  });
+  function mostrarErro(mensagem) {
+    
+    erroNome = document.createElement('p');
+    erroNome.style.color = 'red';
+    erroNome.textContent = mensagem;
+    document.body.appendChild(erroNome);
+  }
+  function removerErro() {
+    if (erroNome) {
+      erroNome.remove();
+      erroNome = null;
+    }
+  }
