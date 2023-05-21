8dd597652483314586abb54d4bd0455020d1c551
rafak7 <rafael.linoseabra@gmail.com>
2023-05-21 03:36:43 -0300

    Add Segundo Nome ao JS

--- js/cp3.js
+++ js/cp3.js
@@ -6,12 +6,30 @@ inputNome.addEventListener("focus", ()=>{
 inputNome.addEventListener("keyup", ()=>{
 
     const lblNome = document.querySelector("label[for='primeiroNome']");
-    if(inputNome.value.length < 5){
+    if(inputNome.value.length < 3){
         inputNome.setAttribute("style","outline-color: #ff0000;");
         lblNome.setAttribute("style","color: #ff0000;");
     }else{
         inputNome.setAttribute("style","outline-color: #00ff00;");
         lblNome.setAttribute("style","color: #00ff00;");
     }
-    //inputEmail.setAttribute("style","outline-color: #ff0000;");
+
+});
+
+const inputSegundoNome = document.querySelector("#segundoNome");
+inputNome.addEventListener("focus", ()=>{
+    inputNome.setAttribute("style","outline-color: #ff0000;");
+})
+
+inputSegundoNome.addEventListener("keyup", ()=>{
+
+    const segundoNome = document.querySelector("label[for='segundoNome']");
+    if(inputSegundoNome.value.length < 3){
+        inputSegundoNome.setAttribute("style","outline-color: #ff0000;");
+        segundoNome.setAttribute("style","color: #ff0000;");
+    }else{
+        inputSegundoNome.setAttribute("style","outline-color: #00ff00;");
+        segundoNome.setAttribute("style","color: #00ff00;");
+    }
+
 });
