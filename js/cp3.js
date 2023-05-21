655911b05d37289cc0bd552fec9874afff3d930a
bruno1098 <rm98470@fiap.com.br>
2023-05-21 02:57:47 -0300

    criando validação de cores no id Primeiro nome

---
+++ js/cp3.js
@@ -0,0 +1,17 @@
+const inputNome = document.querySelector("#primeiroNome");
+inputNome.addEventListener("focus", ()=>{
+    inputNome.setAttribute("style","outline-color: #ff0000;");
+})
+
+inputNome.addEventListener("keyup", ()=>{
+
+    const lblNome = document.querySelector("label[for='primeiroNome']");
+    if(inputNome.value.length < 5){
+        inputNome.setAttribute("style","outline-color: #ff0000;");
+        lblNome.setAttribute("style","color: #ff0000;");
+    }else{
+        inputNome.setAttribute("style","outline-color: #00ff00;");
+        lblNome.setAttribute("style","color: #00ff00;");
+    }
+    //inputEmail.setAttribute("style","outline-color: #ff0000;");
+});
